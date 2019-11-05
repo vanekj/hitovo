344d097238c3b0be0387dabdb19a03871ea83b0b
vanekj <info@jakub-vanek.eu>
2019-11-05 14:32:13 +0100

    feat: Use Buefy framework

--- src/Coffee.vue
+++ src/Coffee.vue
@@ -1,59 +1,72 @@
 <template>
 	<div id="app">
-		<fieldset>
-			<legend>Select the brewing method</legend>
-			<table cellpadding="0" cellspacing="0" style="width: 100%;">
-				<tbody>
-					<tr>
-						<td style="width: 50%">
+		<div class="container">
+			<div class="card">
+				<div class="card-header">
+					<div class="card-header-title">
+						Select the brewing method
+					</div>
+				</div>
+				<div class="card-content">
+					<div class="columns">
+						<div class="column">
 							<label v-for="ratio in items.ratios" :key="ratio.value" style="display: block; padding: 3px 0;">
-								<input name="ratio" type="radio" :value="ratio.value" :checked="ratio.value === selected.ratio.value" @change="calculateRatio(ratio)" />
-								<strong>&nbsp;{{ ratio.label }}</strong>
+								<b-radio v-model="selected.ratio" name="ratio" :native-value="ratio.value" @input="calculateRatio(ratio)">
+									<span :class="{ 'has-text-weight-medium': selected.ratio === ratio.value }">{{ ratio.label }}</span>
+								</b-radio>
 							</label>
-						</td>
-						<td style="width: 50%;">
+						</div>
+						<div class="column is-hidden-mobile">
 							<div class="coffee-wrapper">
 								<transition-group name="coffee-image-switch">
-									<img v-for="ratio in items.ratios" v-show="ratio.value === selected.ratio.value" :key="ratio.value" :src="`/assets/coffee/${ratio.image}.svg`" />
+									<img v-for="ratio in items.ratios" v-show="ratio.value === selected.ratio" :key="ratio.value" :src="`/assets/${ratio.image}.svg`" />
 								</transition-group>
 							</div>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</fieldset>
-		<fieldset>
-			<legend>Ratio</legend>
-			<div style="font-size: 200%; font-weight: bold; text-align: center;">{{ selected.ratio.value }}</div>
-		</fieldset>
-		<table cellpadding="0" cellspacing="0" style="width: 100%;">
-			<tbody>
-				<tr>
-					<td style="width: 50%">
-						<fieldset>
-							<legend>Coffee</legend>
-							<input v-model="calculated.coffee" type="number" min="1" style="width: 100%; font-size: 200%; font-weight: bold; text-align: center;" @input="calculateWater" />
-						</fieldset>
-					</td>
-					<td style="width: 50%">
-						<fieldset>
-							<legend>Water</legend>
-							<input v-model="calculated.water" type="number" min="1" style="width: 100%; font-size: 200%; font-weight: bold; text-align: center;" @input="calculateCoffee" />
-						</fieldset>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<center>
-			<br />
-			<img src="/assets/letters/c_h.png" alt="H" height="32" />
-			<img src="/assets/letters/c_i.png" alt="I" height="32" />
-			<img src="/assets/letters/c_t.png" alt="T" height="32" />
-			<img src="/assets/letters/c_o.png" alt="O" height="32" />
-			<img src="/assets/letters/c_v.png" alt="V" height="32" />
-			<img src="/assets/letters/c_o.png" alt="O" height="32" />
-			<img src="/assets/letters/c_exclamation.png" alt="!" height="32" />
-		</center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="card">
+				<div class="card-header">
+					<div class="card-header-title">
+						Ratio
+					</div>
+				</div>
+				<div class="card-content">
+					<b-input :value="selected.ratio" size="is-medium" custom-class="has-text-centered" expanded disabled />
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="columns">
+				<div class="column">
+					<div class="card">
+						<div class="card-header">
+							<div class="card-header-title">
+								Coffee
+							</div>
+						</div>
+						<div class="card-content">
+							<b-numberinput v-model="calculated.coffee" type="dark" min="1" controls-position="compact" size="is-medium" expanded @input="calculateWater" />
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<div class="card">
+						<div class="card-header">
+							<div class="card-header-title">
+								Water
+							</div>
+						</div>
+						<div class="card-content">
+							<b-numberinput v-model="calculated.water" type="dark" min="1" controls-position="compact" size="is-medium" expanded @input="calculateCoffee" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -114,7 +127,7 @@
 			calculateRatio(selectedRatio) {
 				let [first, second] = selectedRatio.value.split(':');
 				this.calculated.ratio = first / second;
-				this.selected.ratio = selectedRatio;
+				this.selected.ratio = selectedRatio.value;
 			},
 			calculateWater() {
 				this.calculated.water = Math.round(this.calculated.coffee / this.calculated.ratio);
@@ -127,25 +140,41 @@
 </script>
 
 <style lang="postcss">
-	#app {
-		margin: 1rem auto;
-		max-width: 450px;
-		font-family: system-ui;
+	html {
+		height: 100%;
+	}
+
+	body {
+		min-height: 100%;
 		user-select: none;
 	}
 
+	#app {
+		margin: 0 auto;
+		max-width: 500px;
+		padding-right: 0.75rem;
+		padding-left: 0.75rem;
+	}
+
+	.container {
+		padding-top: 0.75rem;
+		padding-bottom: 0.75rem;
+	}
+
 	.coffee-wrapper {
-		position: relative;
-		height: 130px;
-		text-align: center;
-		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
 
 		& span {
+			position: relative;
 			display: flex;
 			justify-content: center;
 			align-items: center;
 			width: 100%;
-			height: 100%;
+			height: 130px;
+			overflow: hidden;
 		}
 
 		& img {
@@ -168,5 +197,9 @@
 
 	.coffee-image-switch-leave-active {
 		transform: translateX(-100px);
+	}
+
+	.letter-image {
+		height: 24px;
 	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
 import Coffee from './Coffee.vue';
+
+Vue.use(Buefy);
 
 new Vue({
 	render: (h) => h(Coffee)
