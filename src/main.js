af0fd2a9efa762d7e33fc1bb9eb59e3a1378495a
vanekj <info@jakub-vanek.eu>
2019-11-06 12:48:01 +0100

    chore: Rename the project

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
-import Coffee from './Coffee.vue';
+import Hitovo from './Hitovo.vue';
 
 Vue.use(Buefy);
 
 new Vue({
-	render: (h) => h(Coffee)
+	render: (h) => h(Hitovo)
 }).$mount('#app');
